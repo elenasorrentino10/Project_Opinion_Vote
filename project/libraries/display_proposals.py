684aa5ba058712c3d12d95309d4deb52d66c2944
LeonardoRocca-13 <leo.rocca03@gmail.com>
2023-06-29 16:58:56 +0200

    Change display and insert proposals functionality.

--- project/libraries/display_proposals.py
+++ project/libraries/display_proposals.py
@@ -3,19 +3,25 @@ import json
 
 
 def display_proposals(r: redis.Redis):
-    proposal_keys = r.keys('proposal:*')
-    proposal_keys.reverse()
-
-    if proposal_keys:
-
-        proposals = [r.hgetall(proposal) for proposal in proposal_keys]
-        print(proposals)
-
-        #proposals = [proposal[b'text'].decode('utf-8') for proposal in proposals]
-
-        #proposals = [f'{index + 1}) {proposal} ' for index, proposal in enumerate(proposals)]
-
-        #print('Proposals:')
-        #print('\n'.join(proposals))
-    else:
+    proposals = r.keys('proposal:*')
+    if not proposals:
         print('No proposals found!')
+        return
+    proposals = [r.hgetall(proposal) for proposal in proposals]
+    proposals = [proposal[b'text'].decode('utf-8') for proposal in proposals]
+
+    users = r.keys('user:*')
+    if not users:
+        print('No users found!')
+        return
+    users = [r.hgetall(user) for user in users]
+    users = [user[b'username'].decode('utf-8') for user in users]
+
+    votes = r.zrange('votes', 0, -1, withscores=True)
+    if not votes:
+        print('No votes found!')
+        return
+
+    for index, data in enumerate(zip(proposals, users, votes)):
+        proposal, user, vote = data
+        print(f'Proposal {index} | User: {user} | Votes: {vote} \n {proposal}')
--- project/libraries/insert_proposal.py
+++ project/libraries/insert_proposal.py
@@ -1,4 +1,3 @@
-import json
 import redis
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -6,17 +5,21 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def insert_proposal(r: redis.Redis, user_id: int):
     proposal_id = r.zcard('votes') + 1
-
     proposal = input('Insert your proposal: ').lower().strip()
 
     if find_similarity(r, proposal):
         print('This proposal is too similar to another one. Please try again.')
-    else:
-        r.hset(f'proposal:{proposal_id}', 'user', user_id)
-        r.hset(f'proposal:{proposal_id}', 'text', proposal)
-        r.zincrby('votes', 1, proposal_id)
-        r.lpush(f'proposal_votes-{proposal_id}', user_id)
-        print('Proposal saved successfully!')
+        choice = input('Do you want to save it anyway? (y/n) ').lower().strip()
+
+        if choice != 'y':
+            print('Proposal not saved!')
+            return
+
+    r.hset(f'proposal:{proposal_id}', 'user', user_id)
+    r.hset(f'proposal:{proposal_id}', 'text', proposal)
+    r.zincrby('votes', 1, proposal_id)
+    r.lpush(f'proposal_votes-{proposal_id}', user_id)
+    print('Proposal saved successfully!')
 
 
 def find_similarity(r: redis.Redis, proposal: str) -> bool:
@@ -31,4 +34,4 @@ def find_similarity(r: redis.Redis, proposal: str) -> bool:
         vectorizer = CountVectorizer().fit_transform(proposals)
         similarity = cosine_similarity(vectorizer[-1], vectorizer[:-1])
 
-        return similarity[0][0] > 0.6
+        return similarity[0][0] > 0.2
